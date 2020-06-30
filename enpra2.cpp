aa6ca524d2b5ee1386a7c599aea40c5e90c73c96
tetsu2536 <tetsu11223311@gmail.com>
2020-06-30 19:49:36 +0900

    create enpra.cpp

--- enpra2.cpp
+++ enpra2.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main(){
-    string a,b;
-    a="言葉を入力";
+    string c,d;
+    c="言葉を入力";
     cout<<"言葉を入力"<<endl;
-    cin>>b;
-    cout<<a+b<<"言葉を入力"<<endl;
+    cin>>d;
+    cout<<c+d<<"言葉を入力"<<endl;
     return 0;
 }
